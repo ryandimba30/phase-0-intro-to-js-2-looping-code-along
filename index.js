94fc757c3a65c1dcb80c05f7fb534e8d62e183c7
ryandimba <ryan.dimba@student.moringaschool.com>
2022-08-13 15:32:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+for (let age =30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+function writeCards(names, event) {
+    let message = [];
+    for (let i =0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+console.logwriteCards(["Ada", "Brendan", "Ali"], "birthday");
+function countDown(number) {
+         while (number >=0) {
+                 console.log(number);
+                 number--;
+         }
+}
